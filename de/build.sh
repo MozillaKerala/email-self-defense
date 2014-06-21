ad7ef2107b5235913ba5ba9bee3896c381018b5d
Gabriel Pérez-Cerezo <gabriel@gpcf.eu>
2014-06-21 17:33:50 +0200

    de: finished next_steps.html

--- de/build.sh
+++ de/build.sh
@@ -6,3 +6,4 @@ cat head.html guide.head.html top-bar-gnu.html index.html.pre gnu-step.html rest
 cat head.html guide.head.html top-bar-mac.html index.html.pre mac-step.html rest.html footer.html > mac.html
 #cat head.html guide.head.html top-bar-win.html index.html.pre win-step.html rest.html footer.html > windows.html
 cat head.html infographic.html.pre footer.html > infographic.html
+cat head.html next_steps.html.pre footer.html > next_steps.html
